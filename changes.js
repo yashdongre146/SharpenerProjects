810c46b1b3002c8bbdae2a194dc851fa52e8dc17
Yash Dongre <83724357+yashdongre146@users.noreply.github.com>
2023-10-20 17:29:32 +0530

    Create changes.js

---
+++ changes.js
@@ -0,0 +1,6 @@
+//changes to youtuber HTML file
+const headerTitle = document.getElementById("header-title");
+const addItems = document.getElementsByClassName("title");
+
+headerTitle.style.border = "2px solid black";
+addItems.innerHTML = "<b>Add Items</b>"
